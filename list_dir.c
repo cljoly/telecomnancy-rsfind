117c14565497040326011e9a980072c05b3fa5c3
Clément Joly <clement.joly@telecomnancy.eu>
2018-10-13 21:19:17 +0200

    Simple command to list content of current directory
    Just to know the API, more to come (structure to repersent files,
    functions…)

---
+++ list_dir.c
@@ -0,0 +1,13 @@
+#include <dirent.h>
+#include <stdio.h>
+
+// TODO Delete this
+int main() {
+  char working_dir_name[] = ".";
+  DIR *working_dir = opendir(working_dir_name);
+  struct dirent *file;
+  // TODO Treat errno after while loop
+  while ((file=readdir(working_dir)) != NULL) {
+    printf("%s/%s\t\t\t%i\t%i\n", working_dir_name, file->d_name, file->d_type, file->d_reclen);
+  }
+}
