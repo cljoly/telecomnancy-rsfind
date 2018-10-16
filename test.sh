2107068ab66eb7eb4f37d70231672828a0076a4c
Clément Joly <clement.joly@telecomnancy.eu>
2018-10-16 21:13:13 +0200

    Add simple test script
    Test rsfind by comparing its output with the one of find. WARNING: Test
    may not pass even if everything is correct. Don’t forget to double-check
    at the start.

---
+++ test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+folder="."
+name="name"
+command="echo"
+rsfind_bin=$1
+rsfind=(
+"echo Testing test script"
+"$rsfind_bin $folder" 
+"$rsfind_bin $folder --name $name"
+"$rsfind $folder --print"
+"$rsfind $folder -t $name"
+"$rsfind $folder -i"
+"$rsfind $folder -l"
+"$rsfind $folder --exec \"$command\""
+) 
+origf=(
+"echo Testing test script"
+"find $folder"
+"find $folder -name $name"
+"find $folder -print"
+"grep -r -l $folder -e $name"
+"find $folder -type f -exec file \; | grep image | cut -d : -f 1"
+"find $folder -l"
+"find $folder -exec \"$command\" \;"
+)
+a=/tmp/rstest_a
+b=/tmp/rstest_b
+
+RED='\033[1;31m'
+GRN='\033[0;32m'
+NC='\033[0m' # No Color
+
+retcode=0
+for i in $(seq 0 3); do
+  ${rsfind[$i]} >$a
+  ${origf[$i]} >$b
+
+  cmp $a $b 2>/dev/null >/dev/null
+  equal=$?
+  if [ $equal -eq 0 ]; then
+    printf "${GRN}TEST $i: ✅${NC}\n"
+    diff $a $b
+  else
+    printf "${RED}TEST $i: ❌${NC} ${rsfind[$i]}\n"
+    diff $a $b
+    retcode=1
+  fi
+  rm $a $b
+done
+exit $retcode
