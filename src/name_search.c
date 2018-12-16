5d358b39218949156b961bf6db6c6f9de2a79604
Clément Joly <clement.joly@telecomnancy.eu>
2018-12-16 11:46:24 +0100

    Option --ename

--- src/name_search.c
+++ src/name_search.c
@@ -5,9 +5,32 @@ filter_result name_filter(context *ctxt, char *path, int is_folder, char *name) 
   if (strncmp(path, name, DNAME_LENGTH) == 0) { // Filename equal name argument
     return FILTER_KEEP;
   } else { if (is_folder) {
-    return FILTER_CONTINUE;
-  } else {
-    return FILTER_IGNORE;
+      return FILTER_CONTINUE;
+    } else {
+      return FILTER_IGNORE;
+    }
   }
+}
+
+filter_result ename_filter(context *ctxt, char *path, int is_folder, char *glob) {
+  fprintf(stderr, "ctxt, is_folder not used: %s %i\n", ctxt->dir_name, is_folder);
+  switch (fnmatch(glob, path, 0)) {
+  case 0: { // Filename matches
+    fprintf(stderr, "0, %s/%s", ctxt->dir_name, path);
+    return FILTER_KEEP;
+    break;
+  }
+  case FNM_NOMATCH: {
+    fprintf(stderr, "FNM_NOMATCH, %s/%s", ctxt->dir_name, path);
+    if (is_folder)
+      return FILTER_CONTINUE;
+    return FILTER_IGNORE;
+    break;
+  }
+  default: {
+    fprintf(stderr, "Error with fnmatch, %s/%s", ctxt->dir_name, path);
+    return FILTER_IGNORE;
+    break;
+    }
   }
 }
--- src/name_search.h
+++ src/name_search.h
@@ -1,4 +1,10 @@
 #include "list_dir.h"
+#include <fnmatch.h>
 
 // Filter file where name is equal to the extra argument
 filter_result name_filter(context*, char*, int, char*);
+
+// Filter file where the name matches the extra argument regexp
+// Works much like the --name option of find, so directories are displayed if it
+// matches
+filter_result ename_filter(context*, char*, int, char* regexp);
